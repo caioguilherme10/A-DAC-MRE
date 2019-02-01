f24382dacde9fbf98e1c71a0489179b766dd2225
MurilloTavares <murillo_eum@hotmail.com>
2019-02-01 16:28:06 -0300

    configurando persistence, pom e persistindo na classe main

--- src/main/java/com/mycompany/mavenproject1/main.java
+++ src/main/java/com/mycompany/mavenproject1/main.java
@@ -5,10 +5,36 @@
  */
 package com.mycompany.mavenproject1;
 
+import com.mycompany.mavenproject1.Cenario1.*;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author IFPB
  */
 public class main {
+    
+    public static void main(String[] args) {
+                
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyAppPU");
+        EntityManager em = emf.createEntityManager();
+
+        Funcionario joao = new Funcionario("Joao", new RG("111"), new CPF("111"), 0);
+        Projeto projetoA = new Projeto("projeto desc.", "Area A", new Date(), new Date());
+        Alocacao alocacao = new Alocacao(
+                joao, projetoA, new Date(), 40
+        );
+        
+        
+        em.getTransaction().begin();
+        em.persist(joao);
+        em.persist(projetoA);
+        em.persist(alocacao);
+        em.getTransaction().commit();
+        
+    }
     
 }
